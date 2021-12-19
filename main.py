ffdbf13a0c80451ddb05ebe992537e0857c60461
Jakub Szuber <jszuber06@gmail.com>
2021-12-19 22:19:24 +0100

    added some window launcher functions to the main function and match-case statement

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import functions as funs
+
 def main():
-    pass
+    leader_board = []  # TODO here will be successively saved users with their result and place in the table
+    # TODO !: maybe the leader board should be done in the form of a class, not a list!
+
+    funs.cleaner()
+    funs.start_window()
+    funs.cleaner()
+    funs.sing_in_window()
+    match funs.start_window():
+        case 1:
+            funs.exec_quiz()
+        case 2:
+            funs.ball_game()
+        case 3:
+            funs.applications()
 
 
 if __name__ == '__main__':
