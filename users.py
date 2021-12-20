4e3a15b3eebdc54c207d10825bede440c8c1dced
Jakub Szuber <jszuber06@gmail.com>
2021-12-20 19:26:52 +0100

    added the most important attributes to methods of the class

--- users.py
+++ users.py
@@ -1,9 +1,11 @@
 class User:
-    def __init__(self):
-        pass
+    def __init__(self, nick, level):
+        self.nick = nick
+        self.level = level
+        self.points = 0
 
-    def __call__(self, *args, **kwargs):
-        pass
+    def __call__(self, amounts_of_points):
+        self.points += amounts_of_points
 
     @staticmethod
     def todo():
