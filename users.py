712653ee5cac2970a5ac175a6734a8e0676765b2
Jakub Szuber <jszuber06@gmail.com>
2021-12-23 19:13:05 +0100

    added methods __str__, __iadd__ and changed method __call__

--- users.py
+++ users.py
@@ -1,11 +1,18 @@
 class User:
-    def __init__(self, nick, level):
+    def __init__(self, nick, level='normal'):
         self.nick = nick
         self.level = level
         self.points = 0
 
-    def __call__(self, amounts_of_points):
+    def __call__(self, new_nick):  # Method which allows to change nick
+        self.nick = new_nick
+
+    def __iadd__(self, amounts_of_points):  # Method which allows to add points
         self.points += amounts_of_points
+        return self
+
+    def __str__(self):
+        print(f'User {self.nick} for now have: {self.points} points')
 
     @staticmethod
     def todo():
