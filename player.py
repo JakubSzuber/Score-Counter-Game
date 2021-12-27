c434870daec5e81723574567597000dda39af73f
Jakub Szuber <jszuber06@gmail.com>
2021-12-27 22:27:50 +0100

    finished methods: card_generator and report_marriage

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
-import itertools  # todo do not import whole module
-import random
+from random import shuffle
+from time import sleep
 
 from users import User
 
@@ -9,13 +9,51 @@ class Player(User):
         self.deck = deck
         super().__init__(nick, level)
 
-    # todo meybe add here decorator
-    def give_card(self):  # Generator which return next card from deck
-        pass
+    def card_generator(self):  # Generator which return next card from deck
+        for i in range(len(self.deck)):
+            yield self.deck[i]
+        else:
+            print('All cards were given!')
 
-    # todo meybe add here decorator
     def report_marriage(self):  # Method which inform about marriage
-        pass
+        clover = 0
+        tile = 0
+        piker = 0
+        heart = 0
+        for dictionary in self.deck:
+            if ('clover', 'queen') in dictionary.keys():
+                clover += 1
+            if ('clover', 'king') in dictionary.keys():
+                clover += 1
+            if ('tile', 'queen') in dictionary.keys():
+                tile += 1
+            if ('tile', 'king') in dictionary.keys():
+                tile += 1
+            if ('piker', 'queen') in dictionary.keys():
+                piker += 1
+            if ('piker', 'king') in dictionary.keys():
+                piker += 1
+            if ('heart', 'queen') in dictionary.keys():
+                heart += 1
+            if ('heart', 'king') in dictionary.keys():
+                heart += 1
+
+        if clover == 2:
+            print(f'{self.nick} have clover marriage and getting 60 pomits!\n')
+            sleep(2)
+            self.points += 60
+        if tile == 2:
+            print(f'{self.nick} have tile marriage and getting 80 pomits!\n')
+            sleep(2)
+            self.points += 80
+        if heart == 2:
+            print(f'{self.nick} have heart marriage and getting 100 pomits!\n')
+            sleep(2)
+            self.points += 100
+        if piker == 2:
+            print(f'{self.nick} have piker marriage and getting 40 pomits!\n')
+            sleep(2)
+            self.points += 40
 
     @classmethod
     def player_creator(cls, suit, figures, nick, level):  # method which create instance
@@ -30,14 +68,14 @@ class Player(User):
         for i in dict_all_cards:
             all_cards_list.append({i: dict_all_cards[i]})
 
-        random.shuffle(all_cards_list)
+        shuffle(all_cards_list)
 
         player_1 = cls(all_cards_list[:12], nick, level)
-        player_2 = cls(all_cards_list[:12], nick, level)
+        player_2 = cls(all_cards_list[12:], nick, level)
 
         return [player_1, player_2]
 
-    ending = staticmethod(lambda: print('Good game!'))
+    ending = staticmethod(lambda: print('Thanks for playing, good game!'))
 
     # TODO set some attributes to private
     # TODO create here property (in any way) which print amount of points etc.
