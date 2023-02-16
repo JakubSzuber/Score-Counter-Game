8d4b1bdf199d768902f1110a88a36cad4aaa0b71
Jakub Szuber <jszuber06@gmail.com>
2023-02-16 23:28:56 +0100

    Added sidebar frame (almost finished)
    Functionality with adding user to the leaderboard is not implemented yet.
    
    TODO: make z textbox sticky to the up and bottom

--- demo.py
+++ demo.py
@@ -25,11 +25,83 @@ class MainWindow(customtkinter.CTk):
         super().__init__()
 
         self.title("Score-Counter-Game")
-        self.geometry(f"{1250}x{600}")
-        self.minsize(800, 500)
+        self.geometry(f"{1100}x{580}")
+        self.minsize(900, 460)
 
-        self.welcome_text_1 = customtkinter.CTkLabel(master=self, text="Score-Counter-Game", font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.welcome_text_1.grid(row=0, column=3, pady=(20, 0))
+        # ????configure grid layout (4x4)
+        self.grid_columnconfigure((1, 2, 3, 4), weight=1)
+        #self.grid_columnconfigure((0, 5), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+        #self.grid_rowconfigure((0, 5), weight=1)
+
+        # Sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=3, sticky="ns")
+
+
+
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text=" Leader Board", font=customtkinter.CTkFont(size=28, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+
+        self.textbox = customtkinter.CTkTextbox(self.sidebar_frame, corner_radius=15, border_width=2, bg_color="#2a2a2a")
+        self.textbox.grid(row=1, column=0, pady=10)
+
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=2, column=0, padx=20, pady=(10, 0))
+        self.switch_var = customtkinter.StringVar(value="on")
+        self.appearance_mode_optionemenu = customtkinter.CTkSwitch(self.sidebar_frame, variable=self.switch_var, onvalue="on", offvalue="off", command=self.change_appearance_mode_event, text="Dark")
+        self.appearance_mode_optionemenu.grid(row=3, column=0, padx=20, pady=(5, 25))
+
+        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
+        self.scaling_label.grid(row=4, column=0, padx=20, pady=(10, 0))
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"], command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=5, column=0, padx=20, pady=(5, 25))
+
+
+
+
+
+        # # create main entry and button
+        # self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
+        # self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+        #
+        # self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
+        # self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+        #
+        # # create radiobutton frame
+        # self.radiobutton_frame = customtkinter.CTkFrame(self)
+        # self.radiobutton_frame.grid(row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        # self.radio_var = tkinter.IntVar(value=0)
+        # self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="CTkRadioButton Group:")
+        # self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
+        # self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=0)
+        # self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
+        # self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1)
+        # self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
+        # self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=2)
+        # self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
+        #
+        # set default values
+        self.textbox.configure(state="disabled")
+        self.scaling_optionemenu.set("100%")
+        self.appearance_mode_optionemenu.select()
+
+    def change_appearance_mode_event(self):
+        if self.switch_var.get() == "on":
+            customtkinter.set_appearance_mode("Dark")
+            self.appearance_mode_optionemenu.text="Dark"
+        else:
+            customtkinter.set_appearance_mode("Light")
+            self.appearance_mode_optionemenu.text="Light"
+
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+
+    def sidebar_button_event(self):
+        print("sidebar_button click")
+
 
 # XXX
 class StartWindow(customtkinter.CTk):
